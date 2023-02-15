3e796a862d42743d33cd56fca041b6f2194db095
Daniel Mendler <mail@daniel-mendler.de>
2023-02-15 08:17:04 +0100

    Version 0.13

--- cape.el
+++ cape.el
@@ -5,8 +5,8 @@
 ;; Author: Daniel Mendler <mail@daniel-mendler.de>
 ;; Maintainer: Daniel Mendler <mail@daniel-mendler.de>
 ;; Created: 2021
-;; Version: 0.12
-;; Package-Requires: ((emacs "27.1") (compat "29.1.3.0"))
+;; Version: 0.13
+;; Package-Requires: ((emacs "27.1") (compat "29.1.3.4"))
 ;; Homepage: https://github.com/minad/cape
 
 ;; This file is part of GNU Emacs.
