7bec22b26be62bc0dab0c2a1ba6bc4b33fcabba1
Daniel Mendler <mail@daniel-mendler.de>
2023-08-14 13:43:03 +0200

    Version 0.17

--- cape.el
+++ cape.el
@@ -5,8 +5,8 @@
 ;; Author: Daniel Mendler <mail@daniel-mendler.de>
 ;; Maintainer: Daniel Mendler <mail@daniel-mendler.de>
 ;; Created: 2021
-;; Version: 0.16
-;; Package-Requires: ((emacs "27.1") (compat "29.1.4.0"))
+;; Version: 0.17
+;; Package-Requires: ((emacs "27.1") (compat "29.1.4.2"))
 ;; Homepage: https://github.com/minad/cape
 ;; Keywords: abbrev, convenience, matching, completion, wp
 
