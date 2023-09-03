b3eb9a8a696c710b51399991a49639f1f8acb584
Daniel Mendler <mail@daniel-mendler.de>
2023-09-04 00:18:54 +0200

    Sort list of Capfs

--- cape.el
+++ cape.el
@@ -30,18 +30,18 @@
 ;; Let your completions fly! This package provides additional completion
 ;; backends in the form of Capfs (completion-at-point-functions).
 ;;
-;; `cape-dabbrev': Complete word from current buffers
+;; `cape-abbrev': Complete abbreviation (add-global-abbrev, add-mode-abbrev).
+;; `cape-dabbrev': Complete word from current buffers.
+;; `cape-dict': Complete word from dictionary file.
 ;; `cape-elisp-block': Complete Elisp in Org or Markdown code block.
-;; `cape-file': Complete file name
-;; `cape-history': Complete from Eshell, Comint or minibuffer history
-;; `cape-keyword': Complete programming language keyword
-;; `cape-elisp-symbol': Complete Elisp symbol
-;; `cape-abbrev': Complete abbreviation (add-global-abbrev, add-mode-abbrev)
-;; `cape-dict': Complete word from dictionary file
-;; `cape-line': Complete entire line from file
-;; `cape-tex': Complete Unicode char from TeX command, e.g. \hbar.
-;; `cape-sgml': Complete Unicode char from SGML entity, e.g., &alpha.
+;; `cape-elisp-symbol': Complete Elisp symbol.
+;; `cape-file': Complete file name.
+;; `cape-history': Complete from Eshell, Comint or minibuffer history.
+;; `cape-keyword': Complete programming language keyword.
+;; `cape-line': Complete entire line from file.
 ;; `cape-rfc1345': Complete Unicode char using RFC 1345 mnemonics.
+;; `cape-sgml': Complete Unicode char from SGML entity, e.g., &alpha.
+;; `cape-tex': Complete Unicode char from TeX command, e.g. \hbar.
 
 ;;; Code:
 
