273dc473a0016f75902d4802101f0f9d46f7b2e3
Daniel Mendler <mail@daniel-mendler.de>
2024-01-02 04:28:51 +0100

    Bump copyright years

--- cape-keyword.el
+++ cape-keyword.el
@@ -1,6 +1,6 @@
 ;;; cape-keyword.el --- Keyword completion function -*- lexical-binding: t -*-
 
-;; Copyright (C) 2021-2023 Free Software Foundation, Inc.
+;; Copyright (C) 2021-2024 Free Software Foundation, Inc.
 
 ;; This file is part of GNU Emacs.
 
